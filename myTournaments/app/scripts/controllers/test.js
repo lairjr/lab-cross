c92b5a08fe02f65d63ad50c0281d2a96ee1aa108
Lair Ipê da Silva Júnior <lairjr@outlook.com>
2015-12-18 17:30:15 -0200

    Added myTournaments application

---
+++ myTournaments/app/scripts/controllers/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myTournamentsApp.controller:TestctrlCtrl
+ * @description
+ * # TestctrlCtrl
+ * Controller of the myTournamentsApp
+ */
+angular.module('myTournamentsApp')
+  .controller('TestctrlCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
