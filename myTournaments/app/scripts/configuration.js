46ed39e085a70bd183423bb4109d225892a07db2
Lair Ipê da Silva Júnior <lairjr@outlook.com>
2016-01-07 13:04:12 -0200

    Added configurations.

---
+++ myTournaments/app/scripts/configuration.js
@@ -0,0 +1,10 @@
+"use strict";
+
+angular.module('myTournamentsApp')
+    .constant('TOURNAMENT_OPTIONS',
+        {
+            schedule: "SCHEDULE",
+            standings: "STANDINGS",
+            stats: "STATS"
+        })
+;
